7d2df2127783c46460801b87cedb68e43276cc99
forkandwait <forkandwait@gmail.com>
2011-08-18 19:21:44 -0700

    Server gets info at command line

--- cgi/server.py
+++ cgi/server.py
@@ -13,18 +13,25 @@
 
     NOTES:  TODO: Add recovery code (try, catch, etc)
     
+'/Users/webbs/PROJECTS/SHELL_ON_SKATES/var/SOS.sqlite3'
+
 **********************************************************************************'''
-## imports
+
+## imports                                                                                                                                        
+import argparse
 import os
 import sys
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
-## Default configs -- I thought 
-os.environ['SOS_DB']='/Users/webbs/PROJECTS/SHELL_ON_SKATES/var/SOS.sqlite3'
-#os.putenv('SOS_DB', '/Users/webbs/PROJECTS/SHELL_ON_SKATES/var/SOS.sqlite3')
-PORT = 9999
+## Handle shell arguments (nifty!)
+parser = argparse.ArgumentParser(description='Run SOS server (which runs SOS cgi...).')
+parser.add_argument('--port', '-p', type=int, help='Port number for server', default=9999)
+parser.add_argument('--db', '-d', type=str, help='Database for CGI')
+args = parser.parse_args()
+os.environ['SOS_DB']=args.db
+PORT = args.port
 
-## set up and fire off cgi server, recovering 
+## set up and fire off cgi server
 class Handler(CGIHTTPRequestHandler):
     cgi_directories = ["/"]
 httpd = HTTPServer(("", PORT), Handler)
