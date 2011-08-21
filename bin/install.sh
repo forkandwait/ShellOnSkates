3a9ea7bad14f423d06d09de363fb664ef9b53f87
forkandwait <forkandwait@gmail.com>
2011-08-20 19:02:52 -0700

    fiddleing install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,10 +1,24 @@
 
-# check that zip is availabe
 
-# test that python3 is available
+
+# check we are in the right place in the dir tree (look for programs,
+# maybe run something with git)
+cd ..
+
+
+# check that zip and python3 are availabe
+
 
 # create database from sql
+DB="$(pwd)/var/SOS.sqlite3"
+sqlite3 $DB < ./share/shellonskates.sql
 
-# prime database from pwd and dir structure where install.sh is located. delete config
+# prime database from pwd and dir structure where install.sh is
+# located. delete config first.  install a simple test program.
+echo "begin; delete from config; insert into config (base_dir) values '$(pwd)'; commit;" | sqlite3 $DB
 
 # output instructions on how to run
+echo "execute the server like this: '$(pwd)/bin/server.py -d $DB'"
+echo "execute the queue runner like this (from cron if wanted): '$(pwd)/bin/qr.py -d $DB' "
+echo ""
+echo "Read the README for more instrutctions."
