9a6eee12350ad20644a42d89583e0f92c4b111f5
forkandwait <forkandwait@gmail.com>
2011-08-21 13:17:52 -0700

    fiddling

--- bin/install.sh
+++ bin/install.sh
@@ -15,11 +15,12 @@ command -v python3 &>/dev/null || { echo "SOS requires python3 but it's not inst
 # create database from sql and prime from current location
 DB="$BASEDIR/var/SOS.sqlite3"
 cat ./share/shellonskates.sql | sqlite3 $DB
-echo "begin; delete from config; insert into config (base_dir) values ('$BASEDIR/tmp'); commit;" | sqlite3 $DB
+echo "begin; delete from config; insert into config (base_dir) values ('$BASEDIR/var/tmp'); commit;" | sqlite3 $DB
 
 # output instructions on how to run
-printf "SUCCESS\n\n"
+printf "SUCCESS!!\n\n"
 printf "add an analysis with \"addanalyis.py\":\n\t 'python3 $BASEDIR/bin/addanalysis.py --help'\n\n"
 printf "execute the server like this:\n\t 'python3 $BASEDIR/cgi/server.py -d $DB'\n\n"
 printf "execute the queue runner like this (from cron if wanted):\n\t 'python3 $BASEDIR/bin/qr.py -d $DB' \n\n"
+printf "browse to http://localhost:9999/shellonskates.py"
 printf "Read the README for more information.\n"
