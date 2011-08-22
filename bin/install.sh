db9e68b28f34e055e24c78f83755a7e064ec467d
forkandwait <forkandwait@gmail.com>
2011-08-21 21:25:42 -0700

    tweaking

--- bin/install.sh
+++ bin/install.sh
@@ -29,9 +29,9 @@ python3 $BASEDIR/bin/addanalysis.py  -d $BASEDIR/var/SOS.sqlite3 \\
 # execute the server:
 echo browse to http://localhost:9999/shellonskates.py 
 (python3 $BASEDIR/cgi/server.py -d $DB) &
-PY_ID=$$
+PY_ID=\$\$
 read 'Press enter to stop server and run queue runner '
-kill $PY_ID
+kill \$PY_ID
 
 # execute the queue runner (from cron if wanted):
 python3 $BASEDIR/bin/qr.py -d $DB
